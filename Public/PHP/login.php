50b3840376059eca7433b5dd49a5712c28cb4e1c
becakdarurat <sofyant757@gmail.com>
2023-04-21 01:58:00 +0700

    fitur login pakai PDO

--- Public/PHP/login.php
+++ Public/PHP/login.php
@@ -1,18 +1,44 @@
-<?php 
-    require_once '../init.php';
-    class Login {
-      
-        public function userLogin(){
-            if(isset($_POST["username"]) && isset($_POST["password"])){
-                
-            };
-        }
+<?php
+require_once '../../App/Core/Database.php';
 
-        public function __construct()
-        {
+class Login extends Database{
+    private $db;
 
-        }
-        
+    public function __construct() {
+        $this->db = new Database();
     }
 
-    ?>
+    public function LoginUser($username, $password) {
+        // Query untuk menambahkan user baru ke database
+        $query = "SELECT * FROM user ($username, $password) VALUES (:username, :password)";
+
+        // Bind data menggunakan method bind()
+        $this->db->query($query);
+        $this->db->bind(':username', $username);
+        $this->db->bind(':password', $password);
+
+        // Eksekusi query
+        $result = $this->db->resultSet();
+
+        // Cek apakah data berhasil ditambahkan
+        if ($result() > 0) {
+            // Jika berhasil, arahkan ke halaman login atau halaman lain yang sesuai
+            header('Location: ../HTML/Home.html');
+        } else {
+            // Jika gagal, berikan pesan error
+            echo "Registrasi gagal";
+        }
+    }
+}
+
+if(isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Buat object Login
+    $Login = new Login();
+
+    // Panggil method LoginUser()
+    $Login->LoginUser($username, $password);
+}
+?>
--- Public/init.php
+++ Public/init.php
@@ -1,6 +1,2 @@
 <?php 
-    require_once 'Config/Config.php';
-    require_once 'PHP/login.php';
-    require_once 'PHP/Registrasi.php';
-    require_once '../App/Core/Database.php';
 ?>
